846ae8db332093552cb75cec05df40937657d810
Ann-Marie <anniedillier@gmail.com>
2021-01-06 20:22:34 -0800

    stripe

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { useQuery } from "@apollo/react-hooks";
-
 import ProductItem from "../ProductItem";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import spinner from "../../assets/spinner.gif";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -15,22 +15,30 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if (data) {
+    if(data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
+        data.products.forEach((product) => {
+          idbPromise('products', 'put', product);
+        });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+         products: products
+       });
       });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
+    return state.products.filter(product => product.category._id === currentCategory);
   }
 
   return (
@@ -38,21 +46,22 @@ function ProductList() {
       <h2>Our Products:</h2>
       {state.products.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
+            {filterProducts().map(product => (
+                <ProductItem
+                  key= {product._id}
+                  _id={product._id}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                  quantity={product.quantity}
+                />
+            ))}
         </div>
       ) : (
         <h3>You haven't added any products yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      { loading ? 
+      <img src={spinner} alt="loading" />: null}
     </div>
   );
 }
