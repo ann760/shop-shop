61e14bad7bd311805c14eca43ed7ffe4be7287b6
Ann-Marie <anniedillier@gmail.com>
2021-01-07 12:28:14 -0800

    update reducer

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-
-  const [, dispatch] = useStoreContext();
-
+  //const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
   const removeFromCart = item => {
     dispatch({
       type: REMOVE_FROM_CART,
